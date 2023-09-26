830485c33e16e06e60bd765caa74d08bdeb62e8b
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-09-26 12:56:44 +0530

    Add button component ```

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-	return <h1 className="text-red-600">Hello World</h1>;
+	return <Button>Test</Button>;
 }
