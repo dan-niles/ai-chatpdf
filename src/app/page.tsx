4fef25c3de56bb9653d36f0a4df9234e7cfbedec
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-09-27 20:39:45 +0530

    Configure authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,38 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs";
 import Image from "next/image";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
 export default async function Home() {
+	const { userId } = await auth();
+	const isAuth = !!userId;
 	return (
 		<div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
 			<div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
 				<div className="flex flex-col items-center text-center">
 					<div className="flex items-center">
-						<h1 className="mr-3 text-5xl">Chat with any PDF</h1>
+						<h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+						<UserButton afterSignOutUrl="/" />
+					</div>
+					<div className="flex mt-2">
+						{isAuth && <Button>Go to Chats</Button>}
+					</div>
+					<p className="max-w-xl mt-1 test-lg text-slate-600">
+						Join millions of students, researchers and professionals to
+						instantly answer questions and research with AI
+					</p>
+
+					<div className="w-full mt-4">
+						{isAuth ? (
+							<h1>fileUpload</h1>
+						) : (
+							<Link href={"/sign-in"}>
+								<Button>
+									Login to Get Started <LogIn className="w-4 h-4 ml-2" />
+								</Button>
+							</Link>
+						)}
 					</div>
 				</div>
 			</div>
