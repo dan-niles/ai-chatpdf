6b5abad80c966b30bbab3d22490edde65918685c
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-10-07 20:03:27 +0530

    Add code to vectorize pdf

--- src/app/api/create-chat/route.ts
+++ src/app/api/create-chat/route.ts
@@ -1,14 +1,43 @@
+import { db } from "@/lib/db";
+import { chats } from "@/lib/db/schema";
+import { loadS3IntoPinecone } from "@/lib/pinecone";
+import { getS3URL } from "@/lib/s3";
+import { auth } from "@clerk/nextjs";
 import { NextResponse } from "next/server";
 
+// /api/create-chat
 export async function POST(req: Request, res: Response) {
+	const { userId } = await auth();
+	if (!userId) {
+		return NextResponse.json({ error: "unauthorized" }, { status: 401 });
+	}
 	try {
 		const body = await req.json();
 		const { file_key, file_name } = body;
-		return NextResponse.json({ message: "Success" });
-	} catch (error) {
-		console.log(error);
+		console.log(file_key, file_name);
+		await loadS3IntoPinecone(file_key);
+		const chat_id = await db
+			.insert(chats)
+			.values({
+				fileKey: file_key,
+				pdfName: file_name,
+				pdfUrl: getS3URL(file_key),
+				userId,
+			})
+			.returning({
+				insertedId: chats.id,
+			});
+
 		return NextResponse.json(
-			{ error: "Something went wrong" },
+			{
+				chat_id: chat_id[0].insertedId,
+			},
+			{ status: 200 }
+		);
+	} catch (error) {
+		console.error(error);
+		return NextResponse.json(
+			{ error: "internal server error" },
 			{ status: 500 }
 		);
 	}
--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -5,11 +5,14 @@ import { Inbox, Loader2 } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
 import axios from "axios";
-import toast from "react-hot-toast";
-import { useState } from "react";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+
+// https://github.com/aws/aws-sdk-js-v3/issues/4126
 
 const FileUpload = () => {
-	const [uploading, setUploading] = useState(false);
+	const router = useRouter();
+	const [uploading, setUploading] = React.useState(false);
 	const { mutate, isLoading } = useMutation({
 		mutationFn: async ({
 			file_key,
@@ -25,41 +28,38 @@ const FileUpload = () => {
 			return response.data;
 		},
 	});
+
 	const { getRootProps, getInputProps } = useDropzone({
 		accept: { "application/pdf": [".pdf"] },
 		maxFiles: 1,
 		onDrop: async (acceptedFiles) => {
-			// console.log("accepted");
 			const file = acceptedFiles[0];
-			console.log(file);
-
 			if (file.size > 10 * 1024 * 1024) {
-				toast.error("Please upload a file smaller than 10MB");
+				// bigger than 10mb!
+				toast.error("File too large");
 				return;
 			}
 
 			try {
 				setUploading(true);
 				const data = await uploadToS3(file);
+				console.log("meow", data);
 				if (!data?.file_key || !data.file_name) {
-					toast.error("Error uploading file");
+					toast.error("Something went wrong");
 					return;
 				}
 				mutate(data, {
-					onSuccess: (data) => {
-						toast.success(data.message);
-						console.log("success", data);
+					onSuccess: ({ chat_id }) => {
+						toast.success("Chat created!");
+						router.push(`/chat/${chat_id}`);
 					},
-					onError: (error) => {
+					onError: (err) => {
 						toast.error("Error creating chat");
-						console.log("error", error);
+						console.error(err);
 					},
 				});
-				console.log("data", data);
 			} catch (error) {
 				console.log(error);
-				toast.error("Error uploading file");
-				return;
 			} finally {
 				setUploading(false);
 			}
@@ -70,20 +70,21 @@ const FileUpload = () => {
 			<div
 				{...getRootProps({
 					className:
-						"border-dashed border-2 border-gray-50 cursor-pointer rounded-xl py-8 flex justify-center items-center flex-col ",
+						"border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
 				})}
 			>
-				<input className="hidden" {...getInputProps} />
+				<input {...getInputProps()} />
 				{uploading || isLoading ? (
 					<>
+						{/* loading state */}
 						<Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
-						<p className="mt-2 text-sm text-slate-400">Sending to GPT</p>
+						<p className="mt-2 text-sm text-slate-400">Sending to GPT...</p>
 					</>
 				) : (
-					<div className="flex justify-center items-center flex-col">
+					<>
 						<Inbox className="w-10 h-10 text-blue-500" />
-						<p className="mt-2 text-sm text-slate-400">Drop PDF here</p>
-					</div>
+						<p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+					</>
 				)}
 			</div>
 		</div>
