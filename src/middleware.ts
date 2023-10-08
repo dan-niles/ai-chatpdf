50fea24092790cc012dbcefedba05ed47ac9e36c
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-10-08 10:43:38 +0530

    Update middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,12 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
 	publicRoutes: ["/", "/api/webhook", "/chat/:path*"],
-	ignoredRoutes: ["/((?!api|trpc))(_next.*|.+.[w]+$)", "/api/create-chat"],
+	ignoredRoutes: [
+		"/((?!api|trpc))(_next.*|.+.[w]+$)",
+		"/api/create-chat",
+		"/api/chat",
+		"/api/get-messages",
+	],
 });
 
 export const config = {
