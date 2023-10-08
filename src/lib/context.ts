5389a6018db58773fb5dfc5e9033139d8a0bb74f
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-10-08 10:07:58 +0530

    Add chat api route

---
+++ src/lib/context.ts
@@ -0,0 +1,44 @@
+import { Pinecone } from "@pinecone-database/pinecone";
+import { convertToAscii } from "./utils";
+import { getEmbeddings } from "./embeddings";
+
+export async function getMatchesFromEmbeddings(
+	embeddings: number[],
+	fileKey: string
+) {
+	try {
+		const client = new Pinecone({
+			environment: process.env.PINECONE_ENVIRONMENT!,
+			apiKey: process.env.PINECONE_API_KEY!,
+		});
+		const pineconeIndex = await client.index("chatpdf");
+		const namespace = pineconeIndex.namespace(convertToAscii(fileKey));
+		const queryResult = await namespace.query({
+			topK: 5, // Get the top 5 similar vectors
+			vector: embeddings,
+			includeMetadata: true,
+		});
+		return queryResult.matches || [];
+	} catch (error) {
+		console.log("error querying embeddings", error);
+		throw error;
+	}
+}
+
+export async function getContext(query: string, fileKey: string) {
+	const queryEmbeddings = await getEmbeddings(query);
+	const matches = await getMatchesFromEmbeddings(queryEmbeddings, fileKey);
+
+	const qualifyingDocs = matches.filter(
+		(match) => match.score && match.score > 0.7
+	);
+
+	type Metadata = {
+		text: string;
+		pageNumber: number;
+	};
+
+	let docs = qualifyingDocs.map((match) => (match.metadata as Metadata).text);
+	// 5 vectors
+	return docs.join("\n").substring(0, 3000);
+}
